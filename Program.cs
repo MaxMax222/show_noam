7afe44abf8dad9068b54432bb06e879b247658fd
MaxMax222 <126956833+MaxMax222@users.noreply.github.com>
2023-06-03 14:51:48 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Show_Noam_Git;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] mat = new int[10, 10];
+        for (int i = 0; i < mat.GetLength(0); i++)
+        {
+            for (int j = 0; j < mat.GetLength(1); j++)
+            {
+                Console.Write(mat[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
