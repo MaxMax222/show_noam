6d3b8462b6014dade8a8ecfb416f36ce8ebc773f
MaxMax222 <126956833+MaxMax222@users.noreply.github.com>
2023-06-03 15:34:52 +0300

    created the skeleton of the Point class

---
+++ Point.cs
@@ -0,0 +1,14 @@
+namespace Show_Noam_Git
+{
+    public class Point
+    {
+        private double x;
+        private double y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
